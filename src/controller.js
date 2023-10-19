c05d9a54e923ddd315956e1f4e4eeb6aa95c2608
nestorechavarria <nesstor.927@gmail.com>
2023-10-18 21:06:24 -0300

    agregado algunos comentarios de los metodos en controller.js

--- src/controller.js
+++ src/controller.js
@@ -1,28 +1,30 @@
 import {pool} from './database.js';
 
 class LibroController{
+    //getAll <--- trae todo lo que tenemos en la base de datos
     async getAll(req, res){
         const [result] = await pool.query('SELECT * FROM Libros'); 
         res.json(result);
     }
+    //add <--- agrega un elemneto a la base de datos
     async add(req, res){
         const libro = req.body;
         const [result]= await pool.query(`INSERT INTO Libros(nombre, autor, categoria, anio_publicacion, isbn) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn]);
         res.json({"Id insertado": result.insertId});
     }
-
+    //delete <--- elimina un elemneto de la base de datos en base a su isbn
     async delete(req, res){
         const libro = req.body;
         const[result] = await pool.query(`DELETE FROM Libros WHERE isbn=(?)`, [libro.isbn]);
         res.json({"Registro eliminado": result.affectedRows});
     }
-
+    // update <--- actualiza los datos de un elemento de la base de datos
     async update(req, res){
         const libro = req.body;
         const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), anio_publicacion=(?), isbn=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn, libro.id]);
         res.json({"Registros actualizados": result.changedRows});
     }
-
+    // getOne <--- trae un solo elemento de la base de datos en base a su id
     async getOne(req, res){
         const libro = req.body;
         const id_libro = parseInt(libro.id);
