67ec3b3a3e2ad23f9ae7bf742e3f1def8e49f1c5
nestorechavarria <nesstor.927@gmail.com>
2023-10-24 03:31:54 -0300

    agregado try catch

--- src/controller.js
+++ src/controller.js
@@ -3,37 +3,82 @@ import {pool} from './database.js';
 class LibroController{
     //getAll <--- trae todo lo que tenemos en la base de datos
     async getAll(req, res){
+        
         const [result] = await pool.query('SELECT * FROM Libros'); 
         res.json(result);
     }
+
     //add <--- agrega un elemneto a la base de datos
     async add(req, res){
         const libro = req.body;
-        const [result]= await pool.query(`INSERT INTO Libros(nombre, autor, categoria, anio_publicacion, isbn) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn]);
-        res.json({"Id insertado": result.insertId});
+        
+	    try{
+            const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, anio_publicacion, isbn) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn]);
+		
+		    res.json({"ID insertado":result.insertId});
+
+		 
+	    }catch (e){
+
+	        console.log( e);
+		    const Error = e.message;
+		    res.status(400).json({Error });	
+	    }
     }
+
     //delete <--- elimina un elemneto de la base de datos en base a su isbn
     async delete(req, res){
         const libro = req.body;
-        const[result] = await pool.query(`DELETE FROM Libros WHERE isbn=(?)`, [libro.isbn]);
-        res.json({"Registro eliminado": result.affectedRows});
+        
+	    try{
+            const[result] = await pool.query(`DELETE FROM Libros WHERE isbn=(?)`, [libro.isbn]);
+		    res.json({"Registros Eliminados ":result.affectedRows});
+
+	    }catch (e){
+	        console.log( e);
+		    const Error = e.message;
+		    res.status(400).json({Error });	
+
+	    }
     }
+
     // update <--- actualiza los datos de un elemento de la base de datos
     async update(req, res){
         const libro = req.body;
-        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), anio_publicacion=(?), isbn=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn, libro.id]);
-        res.json({"Registros actualizados": result.changedRows});
+        
+       
+        
+	    try{
+            const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), anio_publicacion=(?), isbn=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn, libro.id]);
+		    res.json({"Registros actualizados ":result.changedRows});
+		
+		
+	    }catch (e){
+		
+	        console.log( e);
+		    const Error = e.message;
+		    res.status(400).json({Error });	
+	
+	    }
     }
     // getOne <--- trae un solo elemento de la base de datos en base a su id
     async getOne(req, res){
-        const libro = req.body;
-        const id_libro = parseInt(libro.id);
-        const [result] = await pool.query(`SELECT * FROM Libros WHERE id=(?)`, [id_libro]);
-        if (result[0]!=undefined){
-            res.json(result);
-        }else{
-            res.json({"Error": "No se ha encontrado un Libro con el id especificado"})
+        
+        try {
+            const libro = req.body;
+            const id_libro = parseInt(libro.id);
+            const [result] = await pool.query(`SELECT * FROM Libros WHERE id=(?)`, [id_libro]);
+            
+            if (result[0]!=undefined){
+                res.json(result);
+            }else{
+                res.json({"Error": "No se ha encontrado un Libro con el id especificado"});
+            }
+        } catch(error){
+            
+            console.log('There was an error', error);
         }
+        
         
     }
 }
